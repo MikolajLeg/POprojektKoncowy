0b9dc190f5094fe9cf5a3c9d4d64edf84a14a83a
MikolajLeg <01169515@pw.edu.pl>
2022-05-24 15:16:15 +0200

    dalsz czesc mapy

--- mapa.py
+++ mapa.py
@@ -1,12 +1,10 @@
 
 
+from translate import Translator
 import geopandas as gpd
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
-import matplotlib
 #from shapely.geometry import Point
-
-
 
 
 class MapMaker(FigureCanvasQTAgg):
@@ -30,6 +28,9 @@ class MapMaker(FigureCanvasQTAgg):
         self.__data.plot(ax =self.__ax,color="yellow", edgecolor="red",linewidth=0.4)
         self.__set_limits_on_axes()
         self.__check_countries()
+        self.__prepare_countries()
+        print("test")
+        print(self.__country_data)
 
 
     def __set_limits_on_axes(self):
@@ -38,29 +39,42 @@ class MapMaker(FigureCanvasQTAgg):
 
     def __prepare_countries(self):
         for country in self.__list:
-            dates_and_costs = country.get_dates_and_costs()
-            for k,v in dates_and_costs
-            self.__country_data[country.get_name()]
+            dates_and_costs = country.get_dates_and_cost()
+            country_costs = 0
+            num =0
+            for k,v in dates_and_costs.items():
+                if v == "no data":
+                    continue
+                else:
+                    country_costs +=v
+                    num +=1
+            avg_cost = country_costs/num
+            self.__country_data[country.get_name()] = avg_cost
 
 
     def __check_countries(self):
+        T = Translator()
         for country in self.__list:
             for nuts_name in self.__data.NAME_LATN:
-                 if "/" in nuts_name:
-                      new_nuts_name = nuts_name.split("/")
-                      new_nuts_name = new_nuts_name[1]
-                 else:
-                     new_nuts_name =nuts_name
+                if "/" in nuts_name:
+                    new_nuts_name = nuts_name.split("/")
+                    new_nuts_name = new_nuts_name[1]
+                else:
+                    new_nuts_name = nuts_name
 
-                 if new_nuts_name == country.get_name():
-                     print(" ")
-                     print("THEY ARE SAME")
-                     print(nuts_name)
-                     print(country.get_name())
-                     self.__paint_country(nuts_name)
+                new_nuts_name = T.translate(new_nuts_name)
+                if new_nuts_name == country.get_name():
+                    print(" ")
+                    print("THEY ARE SAME")
+                    print(nuts_name)
+                    print(country.get_name())
+                    self.__paint_country(nuts_name)
+
+
 
     def __paint_country(self,nuts_name):
         region = self.__data[self.__data.NAME_LATN == nuts_name]
-        region.plot(ax=self.__ax, color="blue" ,))
+        region.plot(ax=self.__ax, color="blue")
+
 
 
