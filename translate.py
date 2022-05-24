b4eadb0e5817b10f7c388c840adce3bf0dafba1e
JAKUB <jestemqba70120@wp.pl>
2022-05-24 14:16:54 +0200

    translator do mapki

--- translate.py
+++ translate.py
@@ -1,10 +1,95 @@
 
 class Translator:
 
-    def init(self, name):
+    def Translate(self, name):
         if name == "Eesti":
             name = "Estonia"
-        elif name == ""
+            return name
+        elif name == "Danmark":
+            name = 'Denmark'
+            return name
+        elif name == "Elláda":
+            name = 'Grecja'
+            return name
+        elif name == "España":
+            name = 'Spain'
+            return name
+        elif name == "Hrvatska":
+            name = 'Croatia'
+            return name
+        elif name == "Magyarország":
+            name = 'Hungary'
+            return name
+        elif name == "Ísland":
+            name = 'Iceland'
+            return name
+        elif name == "Italia":
+            name = 'Italy'
+            return name
+        elif name == "Lietuva":
+            name = 'Lithuania'
+            return name
+        elif name == "Luxembourg":
+            name = 'Luxembourg'
+            return name
+        elif name == "Latvija":
+            name = 'Latvia'
+            return name
+        elif name == "Crna Gora":
+            name = 'Montenegro'
+            return name
+        elif name == "Severna Makedonija":
+            name = 'North Macedonia'
+            return name
+        elif name == "Sverige":
+            name = 'Sweden'
+            return name
+        elif name == "Slovenija":
+            name = 'Slovenia'
+            return name
+        elif name == "Slovensko":
+            name = 'Slovakia'
+            return name
+        elif name == "Türkiye":
+            name = 'Turkey'
+            return name
+        elif name == "Nederland":
+            name = 'Nederlands'
+            return name
+        elif name == "Polska":
+            name = 'Poland'
+            return name
+        elif name == "Shqipëria":
+            name = 'Albania'
+            return name
+        elif name == "România":
+            name = 'Romania'
+            return name
+        elif name == "Österreich":
+            name = 'Austria'
+            return name
+        elif name == "Belgique/België":
+            name = 'Belgium'
+            return name
+        elif name == "Bulgaria":
+            name = 'Denmark'
+            return name
+        elif name == "Schweiz/Suisse/Svizzera":
+            name = 'Switzerland'
+            return name
+        elif name == "Kýpros":
+            name = 'Cyprus'
+            return name
+        elif name == "Česko":
+            name = 'Czechia'
+            return name
+        elif name == "Deutschland":
+            name = 'Germany'
+            return name
+        elif name == "Norge":
+            name = 'Norway'
+            return name
+
 
 
 
