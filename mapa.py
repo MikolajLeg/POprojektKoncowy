c9bd973f0f0a5e08dbeb7804dfd7b0eb7a8bcc65
JAKUB <jestemqba70120@wp.pl>
2022-05-24 15:35:32 +0200

    translator do mapki

--- mapa.py
+++ mapa.py
@@ -3,7 +3,6 @@
 import geopandas as gpd
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
-import matplotlib
 #from shapely.geometry import Point
 
 
@@ -21,13 +20,16 @@ class MapMaker(FigureCanvasQTAgg):
 
     def __init_map(self):
         self.__ax = self.__fig.add_subplot(111)
-        self.__data = gpd.read_file("NUTS_RG_60M_2021_3857_LEVL_0.geojson")
+        print("test")
+        path_to_data = "./NUTS_RG_60M_2021_3857_LEVL_0.geojson"
 
+        self.__data = gpd.read_file(path_to_data)
+        print("test2")
         self.__make_map()
 
     def __make_map(self):
         self.__ax.clear()
-        self.__data.plot(ax =self.__ax,color="yellow", edgecolor="red",linewidth=0.4)
+        self.__data.plot(ax=self.__ax, color="grey", edgecolor="red", linewidth=0.4)
         self.__set_limits_on_axes()
         self.__check_countries()
 
@@ -36,11 +38,11 @@ class MapMaker(FigureCanvasQTAgg):
         self.__ax.set_xlim([-3 * 1e6, 5.25 * 1e6])
         self.__ax.set_ylim([0.4 * 1e7, 1.2 * 1e7])
 
-    def __prepare_countries(self):
-        for country in self.__list:
-            dates_and_costs = country.get_dates_and_costs()
-            for k,v in dates_and_costs
-            self.__country_data[country.get_name()]
+    # def __prepare_countries(self):
+    #     for country in self.__list:
+    #         dates_and_costs = country.get_dates_and_costs()
+    #         for k,v in dates_and_costs
+    #         self.__country_data[country.get_name()]
 
 
     def __check_countries(self):
@@ -61,6 +63,6 @@ class MapMaker(FigureCanvasQTAgg):
 
     def __paint_country(self,nuts_name):
         region = self.__data[self.__data.NAME_LATN == nuts_name]
-        region.plot(ax=self.__ax, color="blue" ,))
+        region.plot(ax=self.__ax, color="blue")
 
 
